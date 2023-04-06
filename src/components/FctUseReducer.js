5aea5fdb0600486f9b84cc3c720db909132e4c6b
AnthonySar <anthony.sarapata@gmail.com>
2023-04-06 17:22:37 +0200

    useReducer return obj

--- src/components/FctUseReducer.js
+++ src/components/FctUseReducer.js
@@ -1,13 +1,17 @@
 import React, { useReducer } from "react";
 
-const initialState = 0;
+const initialState = {count1: 0, count2: 0};
 
 const reducer = (state, action) => {
-  switch(action) {
+  switch(action.type) {
     case "increment": 
-      return state + 1
+      return {...state, count1: state.count1 + action.value}
     case 'decrement':
-      return state - 1
+      return {...state, count1: state.count1 - action.value}
+    case "increment2": 
+      return {...state, count2: state.count2 + action.value}
+    case 'decrement2':
+      return {...state, count2: state.count2 - action.value}
     case 'reset':
       return initialState
 
@@ -20,13 +24,26 @@ const FctUseReducer = () => {
   const [count, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <div>
-      <h2>HOOKS : useReducer</h2>
-      <p>{count}</p>
-      <button className="btn btn-primary m-3" onClick={() => dispatch('increment')}>+</button>
-      <button className="btn btn-danger m-3" onClick={() => dispatch('decrement')}>-</button>
-      <button className="btn btn-success m-3" onClick={() => dispatch('reset')}>Reset</button>
+    <>
+    <h2>HOOKS : useReducer</h2>
+    <div className="row">
+      <div className="col-6">
+        <p>{count.count1}</p>
+        <button className="btn btn-primary m-3" onClick={() => dispatch({type: 'increment', value: 1})}>+</button>
+        <button className="btn btn-danger m-3" onClick={() => dispatch({type: 'decrement', value: 1})}>-</button>
+      </div>
+
+      <div className="col-6">
+        <p>{count.count2}</p>
+        <button className="btn btn-primary m-3" onClick={() => dispatch({type: 'increment2', value: 5})}>+</button>
+        <button className="btn btn-danger m-3" onClick={() => dispatch({type: 'decrement2', value: 5})}>-</button>
+      </div>
+
+      <div className="w-100 mx-0 mb-3">
+        <button className="btn btn-success" onClick={() => dispatch({type: 'reset'})}>Reset</button>
+      </div>
     </div>
+    </>
   )
 }
 
